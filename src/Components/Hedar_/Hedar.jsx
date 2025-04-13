bc12dd181a3338f1ff5be59de706346f0553e147
AnasAhmedFND <anasahmed4402@gmail.com>
2025-04-13 08:35:57 +0600

    hero category hendele

--- src/Components/Hedar_/Hedar.jsx
+++ src/Components/Hedar_/Hedar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hedar = () => {
   return (
-    <section className='flex bg-[#634C9F] py-2 text-white justify-center gap-10 text-[12px] items-center '>
+    <section className='md:flex bg-[#634C9F] py-2 text-white justify-center gap-10 md:text-[12px] text-[10px] items-center text-center'>
       <p className=''>FREE delivery & 40% Discount for next 3 orders! Place your 1st order in.</p>
       <p className='items-center'>Until the end of the sale: <span className='text-lg font-bold'> 47 </span>days <span className='text-lg font-bold'> 06 </span> hours <span className='text-lg font-bold'> 55 </span>  minutes <span className='text-lg font-bold'> 51 </span>  sec.</p>
     </section>
