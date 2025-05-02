f05f928e7070bb51b71d30f883e04cfef66277ad
AnasAhmedFND <anasahmed4402@gmail.com>
2025-05-02 20:13:12 +0600

    sub tutall in cart page

--- src/Components/Cart_/Cart.jsx
+++ src/Components/Cart_/Cart.jsx
@@ -7,6 +7,7 @@ import { clearAllItems, deleteCartItem } from '../Slice_/cart_Slice';
 import { RiDeleteBin2Fill } from "react-icons/ri";
 import { Link } from 'react-router-dom'
 import { SiShopee } from "react-icons/si";
+import { FaSackDollar } from "react-icons/fa6";
 
 const Cart = () => {
   let info = useContext(apiData)
@@ -39,61 +40,73 @@ const Cart = () => {
         <p><IoIosArrowForward /></p>
         <p className='font-bold '>Cart</p>
       </div>
-      {cartData.length > 0 ?
-        <div className="flex flex-col gap-2  p-2  w-[60%] ">
 
-          {cartData.map((item, index) => (
-            <div className="border-2  md:w-full  md:h-[150px] w-[160px] px-2  rounded-xl flex gap-10 items-center relative">
-              <p onClick={() => hendeleRemoveItem(index)} className='absolute w-[30px] h-[30px] bg-[#6d75a7b5] hover:bg-red-400 border rounded-full items-center flex justify-center top-5 left-[70px] text-white  cursor-pointer '><ImCross /></p>
-              <div className="">
-                <img className='md:w-[100px] w-[300px] border mt-2 rounded-md ' src={item.thumbnail} alt="" />
-                <p className='font-bold '> </p>
+      <div className="flex justify-between  ">
+        {cartData.length > 0 ?
+          <div className="flex flex-col gap-2  p-2  w-[60%] ">
+
+            {cartData.map((item, index) => (
+              <div className="border-2  md:w-full  md:h-[150px] w-[160px] px-2  rounded-xl flex gap-10 items-center relative">
+                <p onClick={() => hendeleRemoveItem(index)} className='absolute w-[30px] h-[30px] bg-[#6d75a7b5] hover:bg-red-400 border rounded-full items-center flex justify-center top-5 left-[70px] text-white  cursor-pointer '><ImCross /></p>
+                <div className="">
+                  <img className='md:w-[100px] w-[300px] border mt-2 rounded-md ' src={item.thumbnail} alt="" />
+                  <p className='font-bold '> </p>
+
+
+                </div>
+                <div className="flex  w-full justify-between items-center ">
+                  <div className="">
+                    <h6 className='font-bold '>Price</h6>
+                    <p className='font-bold text-red-500 text-xl mt-4 '>${item.price}   <span className='line-through '>  $ {item.discountPercentage} </span> </p>
+
+                  </div>
+
+                  <div className="">
+                    <h6 className='font-bold '>Quantity</h6>
+                    <p className='mt-4 border flex justify-between '> <span className='bg-[#706570c5] px-2  '>-</span> <span className='bg-[#b0adc755] px-2 '>1 </span> <span className='bg-[#706570c5] px-2' >+</span> </p>
+                  </div>
+
+                  <div className="">
+                    <h5 className='font-bold '>Total</h5>
+                    <p className='mt-4 '>245</p>
+                  </div>
+
+                </div>
 
 
               </div>
-              <div className="flex  w-full justify-between items-center ">
-                <div className="">
-                  <h6 className='font-bold '>Price</h6>
-                  <p className='font-bold text-red-500 text-xl mt-4 '>${item.price}   <span className='line-through '>  $ {item.discountPercentage} </span> </p>
 
-                </div>
-
-                <div className="">
-                  <h6 className='font-bold '>Quantity</h6>
-                  <p className='mt-4 border flex justify-between '> <span className='bg-[#706570c5] px-2  '>-</span> <span className='bg-[#b0adc755] px-2 '>1 </span> <span className='bg-[#706570c5] px-2' >+</span> </p>
-                </div>
-
-                <div className="">
-                  <h5 className='font-bold '>Total</h5>
-                  <p className='mt-4 '>245</p>
-                </div>
-
-              </div>
+            ))}
 
 
-            </div>
+            <button onClick={hendeleClearAll} className='bg-red-500 text-white font-bold py-2 rounded-md cursor-pointer items-center flex justify-center gap-4 group '>Clear All Items <RiDeleteBin2Fill className='text-red-500 group-hover:text-white' /> </button>
+          </div>
+          :
 
-          ))}
+          <div className="py-5">
+            <Link to="/shop">
+              <button className='bg-red-500 text-white font-bold py-2 rounded-md cursor-pointer items-center flex justify-center gap-4 group w-[60%] '>Go to shop <SiShopee /></button>
+            </Link>
 
+          </div>
+        }
 
-          <button onClick={hendeleClearAll} className='bg-red-500 text-white font-bold py-2 rounded-md cursor-pointer items-center flex justify-center gap-4 group '>Clear All Items <RiDeleteBin2Fill className='text-red-500 group-hover:text-white' /> </button>
-        </div>
-        :
+        <div className="border-2 border-dashed w-[35%] h-[200px] rounded-md  ">
+          <p className='flex justify-center text-5xl py-4 text-red-500 '><FaSackDollar /></p>
 
-        <div className="py-5">
-          
-          
-          <Link to="/shop">
-          <button className='bg-red-500 text-white font-bold py-2 rounded-md cursor-pointer items-center flex justify-center gap-4 group w-[60%] '>Go to shop <SiShopee /></button>
-          </Link>
+          <div className="flex gap-2 mt-[0px] font-bold text-2xl px-2 ">
+            <h5 className=' text-green-500 '>Totall =</h5>
+            <p className='text-red-500'>$445</p>
+          </div>
+          <div className="flex gap-2 font-bold text-2xl px-2 ">
+            <h5 className=' text-green-500'>Subtotall =</h5>
+            <p className='text-red-500'>$450</p>
+          </div>
 
-          
-          
-          
+          <button className='bg-green-500 py-2 border rounded-md w-full text-white font-bold mt-[10px] '>Bay Now</button>
         </div>
 
-
-      }
+      </div>
 
     </section>
   )
--- src/Components/Home_/Quality_product.jsx
+++ src/Components/Home_/Quality_product.jsx
@@ -3,6 +3,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import Alpro from './img_home/alpro.jpg'
 import Ball from './img_home/ball.png'
 import Oatbox from './img_home/oatbox_man.png'
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,15 +14,20 @@ const Quality_product = () => {
                 <p className='text-red-500 text-sm '>Only This Week</p>
                 <h4 className='font-bold mt-2 '>Provides you experienced <br />quality products</h4>
                 <p className='text-sm mt-2 '>Feed your family the best</p>
-                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>
+                <Link to='/shop' >
+                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white cursor-pointer '>Shop Now <FaLongArrowAltRight /></button>
+                </Link>
             </div>
 
             <div className="border bg-[#e7f0f0] rounded-sm pt-2 md:w-[250px] w-full md:mt-0 mt-2 ">
                 <div className="px-2">
                 <p className='text-red-500 text-sm '>Only This Week</p>
                 <h4 className='font-bold mt-2 '>Shopping with us for better <br />quality and the best price</h4>
-                <p  className='text-sm mt-2 '>Only this week. Don’t miss...</p>
-                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>
+                <p  className='text-sm mt-2 '>Only this week. Don’t miss...</p> 
+
+                <Link to='/shop' >
+                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>                
+                </Link>
 
                 </div>
                 <img className='md:w-[250px] w-full h-[160px] mt-4 rounded-b-sm' src={Alpro} alt="" />
@@ -31,8 +37,11 @@ const Quality_product = () => {
                 <div className="px-2">
                 <p className='text-red-500 text-sm '>Only This Week</p>
                 <h4 className='font-bold mt-2 '>Get the best quality products <br />at the lowest prices</h4>
-                <p  className='text-sm mt-2 '>A different kind of grocery store</p>
-                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>
+                <p  className='text-sm mt-2 '>A different kind of grocery store</p> 
+
+                <Link to='/shop' >
+                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>                
+                </Link>
 
                 </div>
                 <img className='md:w-[250px] w-full  h-[160px] mt-4 ' src={Ball} alt="" />
@@ -42,8 +51,11 @@ const Quality_product = () => {
                 <div className="px-2 ">
                 <p className='text-red-500 text-sm '>Only This Week</p>
                 <h4 className='font-bold mt-2 '>Where you get your all favorite <br />brands under one roof</h4>
-                <p  className='text-sm mt-2 '>Only this week. Don’t miss...</p>
-                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>
+                <p  className='text-sm mt-2 '>Only this week. Don’t miss...</p> 
+                
+                <Link to='/shop' >
+                <button className='border flex gap-2 items-center rounded-full font-bold px-4 py-1 text-sm mt-3 bg-white '>Shop Now <FaLongArrowAltRight /></button>                
+                </Link>
 
                 </div>
                 <img className='md:w-[250px] w-full  h-[160px] mt-4 ' src={Oatbox} alt="" />
