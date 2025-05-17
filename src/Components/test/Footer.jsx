08bba32a953dd59a08d8c30728554b23e82a1cf8
AnasAhmedFND <anasahmed4402@gmail.com>
2025-05-17 14:39:37 +0600

    text responsive

--- src/Components/test/Footer.jsx
+++ src/Components/test/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
    <section className='border-b-4'>
-    <div className="flex justify-center items-center border bg-[#e0e1e8c0] py-20 ">
-        <a className='text-blue-700 ' href="">anasahmed@gmail.com</a>
-        <input className='border w-[250px] py-4 ml-5 rounded-lg outline-none px-2' type="text" placeholder='some text' />
+    <div className="md:flex justify-center items-center border bg-[#e0e1e8c0] py-20 ">
+        <a className='text-blue-700 md:ml-0 ml-6' href="">anasahmed@gmail.com</a>
+        <input className=' w-[250px] py-4 ml-5 rounded-lg outline-none px-2 md:mt-0 mt-2 ' type="text" placeholder='some text' />
     </div>
    </section>
   )
