726b88e618d96f2e4cff60fef18349c473ed9044
AnasAhmedFND <anasahmed4402@gmail.com>
2025-05-17 15:10:05 +0600

    mail tag

--- src/Components/test/Footer.jsx
+++ src/Components/test/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
    <section className='border-b-4'>
     <div className="md:flex justify-center items-center border bg-[#e0e1e8c0] py-20 ">
-        <a className='text-blue-700 md:ml-0 ml-6' href="">anasahmed@gmail.com</a>
+        <p className='text-blue-700 md:ml-0 ml-6' href="">anasahmed@gmail.com</p>
         <input className=' w-[250px] py-4 ml-5 rounded-lg outline-none px-2 md:mt-0 mt-2 ' type="text" placeholder='some text' />
     </div>
    </section>
