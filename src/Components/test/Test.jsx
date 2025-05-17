0b54f015dc88b4587a1db56056e2b0ad1d96184f
AnasAhmedFND <anasahmed4402@gmail.com>
2025-05-17 14:41:16 +0600

    res

--- src/Components/test/Test.jsx
+++ src/Components/test/Test.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Test = () => {
     return (
         <section className='container mx-auto'>
-            <div className=" flex justify-between mt-5 py-4 gap-2">
-            <div className="w-[70%]   ">
+            <div className=" md:flex justify-between mt-5 py-4 gap-2">
+            <div className="md:w-[70%]   ">
                 <h4 className='text-center font-bold bg-[#b6c2c955] py-10'>Main Content 70% </h4>
 
             </div>
 
-            <div className="w-[30%] ">
+            <div className="md:w-[30%] md:mt-0 mt-2">
                 <h4 className='text-center font-bold bg-[#b6c2c955] py-10'>Right 30%</h4>
             </div>
 
